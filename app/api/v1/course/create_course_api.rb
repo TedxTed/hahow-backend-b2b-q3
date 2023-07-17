4a944e6c6f34134be43beae44d87d7210d2b4fc1
Ted <wodf410@gmail.com>
2023-07-17 20:09:23 +0800

    [add] without model, create course  api

--- app/api/v1/course/create_course_api.rb
+++ app/api/v1/course/create_course_api.rb
@@ -1,7 +1,40 @@
-module V1::Course
-  class CreateCourseApi < Grape::API
-    post '/courses' do
-      { status: 'success' }
+module V1
+  module Course
+    class CreateCourseApi < Grape::API
+      resource :courses do
+        desc 'Create a new course'
+        params do
+          requires :course_info, type: Hash do
+            requires :course_name, type: String, desc: 'Course name'
+            requires :instructor_name, type: String, desc: 'Instructor name'
+            requires :course_description, type: String, desc: 'Course description'
+            requires :chapters, type: Array do
+              requires :chapter_order, type: Integer, desc: 'Chapter order'
+              requires :chapter_name, type: String, desc: 'Chapter name'
+              requires :units, type: Array do
+                requires :unit_order, type: Integer, desc: 'Unit order'
+                requires :unit_name, type: String, desc: 'Unit name'
+              end
+            end
+          end
+        end
+
+        post do
+          course_info = params[:course_info]
+          course_info[:course_id] = SecureRandom.hex(8)
+
+          course_info[:chapters].each do |chapter|
+            chapter[:chapter_id] = SecureRandom.hex(8)
+            next unless chapter[:units]
+
+            chapter[:units].each do |unit|
+              unit[:unit_id] = SecureRandom.hex(8)
+            end
+          end
+
+          { status: 'success', course: course_info }
+        end
+      end
     end
   end
 end
--- spec/api/v1/create_course_api_spec.rb
+++ spec/api/v1/create_course_api_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe 'Courses API', type: :request do
   describe 'POST /api/v1/courses' do
+    def json
+      JSON.parse(response.body)
+    end
+
     let(:units) do
       2.times.map do |i|
         { unit_order: i + 1, unit_name: Faker::Lorem.word }
@@ -13,7 +17,7 @@ describe 'Courses API', type: :request do
         {
           chapter_order: i + 1,
           chapter_name: Faker::Lorem.word,
-          units: i < 2 ? units : []
+          units:
         }
       end
     end
@@ -35,6 +39,7 @@ describe 'Courses API', type: :request do
       before { post '/api/v1/courses', params: valid_attributes }
 
       it 'creates a course' do
+        json = JSON.parse(response.body)
         expect(json['course']['course_name']).to eq(valid_attributes[:course_info][:course_name])
         expect(json['course']['instructor_name']).to eq(valid_attributes[:course_info][:instructor_name])
         expect(json['course']['course_description']).to eq(valid_attributes[:course_info][:course_description])
@@ -46,15 +51,32 @@ describe 'Courses API', type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/api/v1/courses', params: {} } # Changed line
+      before { post '/api/v1/courses', params: {} }
 
-      it 'returns status code 422' do
-        expect(response).to have_http_status(422)
+      it 'returns status code 400' do
+        expect(response).to have_http_status(400)
       end
 
       it 'returns a validation failure message' do
         expect(response.body)
-          .to match(/Validation failed: Course info can't be blank/)
+          .to match(/course_info is missing, course_info\[course_name\] is missing, course_info\[instructor_name\] is missing, course_info\[course_description\] is missing, course_info\[chapters\] is missing/)
+      end
+    end
+
+    context 'when chapter_order is a string' do
+      before do
+        invalid_attributes = valid_attributes.dup
+        invalid_attributes[:course_info][:chapters].first[:chapter_order] = 'a'
+        post '/api/v1/courses', params: invalid_attributes
+      end
+
+      it 'returns status code 400' do
+        expect(response).to have_http_status(400)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match('{"error":"course_info[chapters][0][chapter_order] is invalid"}')
       end
     end
   end
