20ca9a9b46be80432adbf147a7244f04c4ad2b1a
Ted <wodf410@gmail.com>
2023-07-19 22:01:08 +0800

    [deploy] deploy to flyio

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   mount EndpointApi => EndpointApi::PREFIX
-  mount GrapeSwaggerRails::Engine => '/RailsAPI'
 end
